cd83b2bd6a2ebc018f0f262ebb79e98771a0a2c4
Akhilesh Ravuri <akhileshr@tribytetech.com>
2026-04-16 22:00:45 +0530

    NEW TEST

--- InBuiltQuizPractice/QuizTaker/js/env.js
+++ InBuiltQuizPractice/QuizTaker/js/env.js
@@ -5,5 +5,6 @@ const ENV = {
     { name: "Programming", id: "1qKJihERrxvmtYOYr1umKUsZC6nqRJFep" },
     { name: "Question Types", id: "1pWxAQt3eGx940lEfqZ4UNX_C3DGy600T" },
     { name: "Communication", id: "17YCojdsuzn9aq9J2aoCEAi5VJaXnqDpQ" },
+    { name: "Competitive Reasoning", id:"1YZb2bhVpNhpPzRdQNu8Izsbb9t3NmdO4"}
   ]
 };
